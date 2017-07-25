586b2913c991845c2500c988983a48a22cd21c15
taku tada <shonanking@taku-MacBook-Air.local>
2017-07-26 00:07:11 +0900

    use multiboot2

--- mini_os/src/lib.rs
+++ mini_os/src/lib.rs
@@ -9,12 +9,20 @@ mod vga_buffer;
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_information_address: usize) {
     vga_buffer::clear_screen();
     println!("Hello World{}", "!");
-    println!("{}", { println!("inner"); "outer" });
+    
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("  start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
     loop{}
 }
 
---
+++ mini_os/target/debug/deps/mini_os-de89566bd6ca521e.d
@@ -0,0 +1,6 @@
+/Users/shonanking/miniOS/mini_os/target/debug/deps/libmini_os-de89566bd6ca521e.a: src/lib.rs src/vga_buffer.rs
+
+/Users/shonanking/miniOS/mini_os/target/debug/deps/mini_os-de89566bd6ca521e.d: src/lib.rs src/vga_buffer.rs
+
+src/lib.rs:
+src/vga_buffer.rs:
