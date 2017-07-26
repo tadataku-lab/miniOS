4ae449746e8aa01b232c9f5d11281062219672f3
taku tada <shonanking@taku-MacBook-Air.local>
2017-07-26 15:43:34 +0900

    complete allocating frames

--- mini_os/src/lib.rs
+++ mini_os/src/lib.rs
@@ -6,6 +6,9 @@
 #[macro_use]
 mod vga_buffer;
 
+mod memory;
+use memory::FrameAllocator;
+
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
@@ -23,8 +26,46 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     for area in memory_map_tag.memory_areas() {
         println!("  start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
     }
+
+    let elf_sections_tag = boot_info.elf_sections_tag().expect("ELF-sections tag required");
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("  addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section.addr, section.size, section.flags);
+    }
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+    println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
+
+    let mut frame_allocator = memory::area_frame_allocator::AreaFrameAllocator::new(
+        kernel_start as usize, kernel_end as usize, multiboot_start,
+        multiboot_end, memory_map_tag.memory_areas()
+    );
+
+    println!("{:?}", frame_allocator.allocate_frame() );
+
+    for i in 0.. {
+        if let None = frame_allocator.allocate_frame() {
+            println!("allocated {} frames", i);
+            break;
+        }
+    }
+
+
     loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("  {}", fmt);
+    loop{}
+}
--- mini_os/target/x86_64-mini_os/debug/libmini_os.d
+++ mini_os/target/x86_64-mini_os/debug/libmini_os.d
@@ -1,1 +1,1 @@
-/Users/shonanking/miniOS/mini_os/target/x86_64-mini_os/debug/libmini_os.a: /Users/shonanking/miniOS/mini_os/src/lib.rs /Users/shonanking/miniOS/mini_os/src/vga_buffer.rs
+/Users/shonanking/miniOS/mini_os/target/x86_64-mini_os/debug/libmini_os.a: /Users/shonanking/miniOS/mini_os/src/lib.rs /Users/shonanking/miniOS/mini_os/src/memory/mod.rs /Users/shonanking/miniOS/mini_os/src/memory/area_frame_allocator.rs /Users/shonanking/miniOS/mini_os/src/vga_buffer.rs
